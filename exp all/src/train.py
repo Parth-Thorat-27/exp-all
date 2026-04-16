275517e77fe5b7345d0cdb73cf59bf5fc9e90f1e
Parth-Thorat-27 <pt47275@gmail.com>
2026-04-17 04:36:45 +0530

    Exp 2 CI pipeline

---
+++ exp all/src/train.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Load data
+X, y = load_iris(return_X_y=True)
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Save model
+joblib.dump(model, "models/model.pkl")
+
+print("Model trained and saved!")
